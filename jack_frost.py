334c95d20bc44dfbdc7af04734776bace9c9b02e
Caspian <104550474+Plu3d@users.noreply.github.com>
2022-09-04 00:00:27 +1000

    Update jack_frost.py

--- jack_frost.py
+++ jack_frost.py
@@ -4,7 +4,8 @@ mc = minecraft.Minecraft.create()
 always = True
 while always:
     p = mc.player.getTilePos()
-    mc.setBlock(p.x, p.y, p.z, block.SNOW)
+    if mc.getBlock(p.x, p.y, p.z) != 0:
+        mc.setBlock(p.x, p.y, p.z, block.SNOW)
 
     for hit in mc.events.pollBlockHits():
         mc.setBlock(hit.pos.x, hit.pos.y, hit.pos.z, block.ICE)
