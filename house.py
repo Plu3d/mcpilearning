ac7d9d2622aa80b256106dca53440ed7ede754c6
Caspian <caspianlack@gmail.com>
2022-09-04 23:20:44 +1000

    House Skeleton :)

--- house.py
+++ house.py
@@ -49,23 +49,156 @@ mc.setBlocks(p.x + dimx, p.y - 1, p.z + dimz,
 mc.setBlocks(p.x + dimx - 1, p.y, p.z + dimz - 1, 
              p.x - dimx + 1, p.y + 3, p.z - dimz + 1, 
              block.AIR)
+
+mc.setBlocks(p.x + dimx + 1, p.y - 1, p.z + dimz + 1, 
+             p.x + dimx + 1, p.y + dimy + 1, p.z + dimz + 1, 
+             17, 13)#SPRUCE LOG
+
+mc.setBlocks(p.x - dimx - 1, p.y - 1, p.z + dimz + 1, 
+             p.x - dimx - 1, p.y + dimy + 1, p.z + dimz + 1, 
+             17, 13)#SPRUCE LOG 
+
+mc.setBlocks(p.x - dimx - 1, p.y - 1, p.z - dimz - 1, 
+             p.x - dimx - 1, p.y + dimy + 1, p.z - dimz - 1, 
+             17, 13)#SPRUCE LOG              
+
+mc.setBlocks(p.x + dimx + 1, p.y - 1, p.z - dimz - 1, 
+             p.x + dimx + 1, p.y + dimy + 1, p.z - dimz - 1, 
+             17, 13)#SPRUCE LOG
 #DOOR FRAME
 mc.setBlocks(p.x - 2, p.y, p.z + dimz, 
              p.x - 2, p.y + 1, p.z + dimz, 
              block.AIR)
 
-mc.setBlocks(p.x + dimx + 1, p.y - 1, p.z + dimz + 1, 
-             p.x + dimx + 1, p.y + dimy + 1, p.z + dimz + 1, 
-             17, 1)#SPRUCE LOG
+mc.setBlocks(p.x + dimx, p.y - 1, p.z + dimz, 
+             p.x - dimx, p.y - 1, p.z - dimz, 
+             5, 1)   
 
-mc.setBlocks(p.x - dimx - 1, p.y - 1, p.z + dimz + 1, 
-             p.x - dimx - 1, p.y + dimy + 1, p.z + dimz + 1, 
-             17, 1)#SPRUCE LOG 
+mc.setBlocks(p.x + dimx + 1, p.y + dimy, p.z + dimz + 1, 
+             p.x - dimx - 1, p.y + dimy, p.z - dimz - 1, 
+             17, 13)
 
-mc.setBlocks(p.x - dimx - 1, p.y - 1, p.z - dimz - 1, 
-             p.x - dimx - 1, p.y + dimy + 1, p.z - dimz - 1, 
-             17, 1)#SPRUCE LOG              
+mc.setBlocks(p.x + dimx, p.y + dimy, p.z + dimz, 
+             p.x - dimx, p.y + dimy, p.z - dimz, 
+             block.AIR)
 
-mc.setBlocks(p.x + dimx + 1, p.y - 1, p.z - dimz - 1, 
-             p.x + dimx + 1, p.y + dimy + 1, p.z - dimz - 1, 
-             17, 1)#SPRUCE LOG        
+#roof
+rdimx = dimx
+rdimy = dimy
+rdimz = dimz
+for x in range(dimx + 2):
+    mc.setBlocks(p.x - rdimx - 2, p.y + rdimy, p.z + rdimz + 2,
+                 p.x - rdimx - 2, p.y + rdimy, p.z - rdimz - 2,
+                 53, 0)
+
+    mc.setBlocks(p.x - rdimx - 2, p.y + rdimy - 1, p.z + rdimz + 2,
+                 p.x - rdimx - 2, p.y + rdimy - 1, p.z + rdimz + 2,
+                 108, 5)
+    
+    #inner diagonal beam
+    mc.setBlocks(p.x - rdimx - 2, p.y + rdimy - 1, p.z + rdimz + 1,
+                 p.x - rdimx - 2, p.y + rdimy - 1, p.z + rdimz + 1,
+                 17, 13)
+
+    mc.setBlocks(p.x - rdimx - 2, p.y + rdimy, p.z + rdimz + 2,
+                 p.x - rdimx - 2, p.y + rdimy, p.z + rdimz + 2,
+                 108, 0)
+    
+    mc.setBlocks(p.x - rdimx - 2, p.y + rdimy - 1, p.z - rdimz - 2,
+                 p.x - rdimx - 2, p.y + rdimy - 1, p.z - rdimz - 2,
+                 108, 5)
+    
+    #inner diagonal beam
+    mc.setBlocks(p.x - rdimx - 2, p.y + rdimy - 1, p.z - rdimz - 1,
+                 p.x - rdimx - 2, p.y + rdimy - 1, p.z - rdimz - 1,
+                 17, 13)
+    
+    mc.setBlocks(p.x - rdimx - 2, p.y + rdimy, p.z - rdimz - 2,
+                 p.x - rdimx - 2, p.y + rdimy, p.z - rdimz - 2,
+                 108, 0)
+    
+    #other side
+
+    mc.setBlocks(p.x + rdimx + 2, p.y + rdimy, p.z + rdimz + 2,
+                 p.x + rdimx + 2, p.y + rdimy, p.z - rdimz - 2,
+                 53, 1)
+
+    mc.setBlocks(p.x + rdimx + 2, p.y + rdimy - 1, p.z + rdimz + 2,
+                 p.x + rdimx + 2, p.y + rdimy - 1, p.z + rdimz + 2,
+                 108, 4)
+
+    #inner diagonal beam
+    mc.setBlocks(p.x + rdimx + 2, p.y + rdimy - 1, p.z + rdimz + 1,
+                 p.x + rdimx + 2, p.y + rdimy - 1, p.z + rdimz + 1,
+                 17, 13)
+
+    mc.setBlocks(p.x + rdimx + 2, p.y + rdimy, p.z + rdimz + 2,
+                 p.x + rdimx + 2, p.y + rdimy, p.z + rdimz + 2,
+                 108, 1)
+    
+    mc.setBlocks(p.x + rdimx + 2, p.y + rdimy - 1, p.z - rdimz - 2,
+                 p.x + rdimx + 2, p.y + rdimy - 1, p.z - rdimz - 2,
+                 108, 4)
+    
+    #inner diagonal beam
+    mc.setBlocks(p.x + rdimx + 2, p.y + rdimy - 1, p.z - rdimz - 1,
+                 p.x + rdimx + 2, p.y + rdimy - 1, p.z - rdimz - 1,
+                 17, 13)
+    
+    mc.setBlocks(p.x + rdimx + 2, p.y + rdimy, p.z - rdimz - 2,
+                 p.x + rdimx + 2, p.y + rdimy, p.z - rdimz - 2,
+                 108, 1)
+
+    #upper wall
+    mc.setBlocks(p.x + rdimx + 2, p.y + rdimy - 1, p.z + rdimz,
+                 p.x - rdimx - 2, p.y + rdimy - 1, p.z + rdimz,
+                 block.SNOW_BLOCK)
+
+    mc.setBlocks(p.x + rdimx + 2, p.y + rdimy - 1, p.z - rdimz,
+                 p.x - rdimx - 2, p.y + rdimy - 1, p.z - rdimz,
+                 block.SNOW_BLOCK)
+
+    
+
+    rdimx = rdimx - 1
+    rdimy = rdimy + 1
+
+mc.setBlocks(p.x - dimx - 2, p.y + dimy, p.z + dimz + 2,
+                 p.x - dimx - 2, p.y + dimy, p.z - dimz - 2,
+                 108, 0)
+
+#head
+mc.setBlocks(p.x - rdimx - 2, p.y + rdimy - 1, p.z + rdimz + 2,
+                 p.x - rdimx - 2, p.y + rdimy - 1, p.z - rdimz - 2,
+                 45)
+
+#clean up
+mc.setBlocks(p.x - dimx - 2, p.y + dimy - 1, p.z + dimz + 2,
+             p.x - dimx - 2, p.y + dimy - 1, p.z - dimz - 2,
+             block.AIR)
+
+mc.setBlocks(p.x - dimx - 1, p.y + dimy - 1, p.z + dimz,
+             p.x - dimx - 1, p.y + dimy - 1, p.z - dimz,
+             block.AIR)
+
+mc.setBlocks(p.x + dimx + 2, p.y + dimy - 1, p.z + dimz + 2,
+             p.x + dimx + 2, p.y + dimy - 1, p.z - dimz - 2,
+             block.AIR)
+
+
+mc.setBlocks(p.x + dimx + 1, p.y + dimy - 1, p.z + dimz,
+             p.x + dimx + 1, p.y + dimy - 1, p.z - dimz,
+             block.AIR)
+
+#frame more beams and remove 4 wall
+mc.setBlocks(p.x + dimx + 1, p.y + dimy, p.z + dimz + 1, 
+             p.x - dimx - 1, p.y + dimy, p.z - dimz - 1, 
+             17, 13)
+
+mc.setBlocks(p.x, p.y + dimy + 1, p.z + dimz + 1,
+             p.x, p.y + dimy + dimx, p.z + dimz + 1,
+             17, 13)
+
+mc.setBlocks(p.x, p.y + dimy + 1, p.z - dimz - 1,
+             p.x, p.y + dimy + dimx, p.z - dimz - 1,
+             17, 13)
